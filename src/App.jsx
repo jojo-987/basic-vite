c475899734e4bdbf2c196c48db1f313e6bcc36c7
jojo-987 <polnareff234jojo@gmail.com>
2026-03-03 12:35:07 +0530

    basic vite uploaded

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="main-page">
+      Hello Intern 2026, This is version number 1.
+      </div>
+    </>
+  )
+}
+
+export default App
